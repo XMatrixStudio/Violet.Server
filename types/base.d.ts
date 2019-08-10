a77965167a1d98b08e7b3ec13fa94fc897196247
MegaShow <megaxiu@outlook.com>
2019-08-10 18:24:47 +0800

    Use framework nestjs

--- types/base.d.ts
+++
@@ -1,9 +0,0 @@
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-
-type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> & { [K in Keys]-?: Required<Pick<T, K>> }[Keys]
-
-type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
-  { [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>> }[Keys]
-
-type OnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
-  { [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>> }[Keys]
--- types/github-reserved-names.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'github-reserved-names' {
-  function check(username: string): boolean
-}
