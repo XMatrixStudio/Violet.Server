6e9a13e5fecdd7d1ac300c05564b2cc34156e870
MegaShow <megaxiu@outlook.com>
2019-09-26 00:22:11 +0800

    Add util modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 
 import { UserModule } from './modules/user/user.module'
+import { UtilModule } from './modules/util/util.module'
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, UtilModule],
 })
 export class AppModule {}
