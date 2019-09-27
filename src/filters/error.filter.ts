16cf38ad2dc61e07e8c638f5d65abed158f9bca6
MegaShow <megaxiu@outlook.com>
2019-09-27 22:03:53 +0800

    Add git attributes

--- src/filters/error.filter.ts
+++ src/filters/error.filter.ts
@@ -1,14 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common'
 import { Response } from 'express'
 
-@Catch()
-export class HttpExceptionFilter implements ExceptionFilter {
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter<HttpException> {
   catch(exception: HttpException, host: ArgumentsHost): Response {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse<Response>()
     const status = exception.getStatus() || HttpStatus.INTERNAL_SERVER_ERROR
     const errJson = exception.getResponse()
-
     return response.status(status).json(errJson)
   }
 }
