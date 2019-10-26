b234dd6aaff7b981023dbeb414ef180f79ae0883
MegaShow <megaxiu@outlook.com>
2019-10-26 14:14:12 +0800

    Fix tsc build

--- packages/definitely-typed/types/captchapng2/index.d.ts
+++ packages/definitely-typed/types/captchapng2/index.d.ts
@@ -1,10 +1,23 @@
 declare module 'captchapng2' {
-  import * as PngLib from 'node-pnglib'
-
   namespace CaptchaPng {}
 
   class CaptchaPng extends PngLib {
-    constructor(width: number, height: number, dispNumer: number)
+    constructor(width: number, height: number, displayNumber: number)
+  }
+
+  class PngLib {
+    constructor(width: number, height: number, depth?: number, bgColor?: string | Array<number>)
+
+    index(x: number, y: number): number
+    color(rgba: string | Array<number>): number
+
+    setBgColor(rgba: string | Array<number>): number
+    setPixel(x: number, y: number, rgba: string | Array<number>): void
+
+    getBase64(): string
+    getBuffer(): Buffer
+
+    deflate(): Buffer
   }
 
   export = CaptchaPng
